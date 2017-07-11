2a8083341935927632365e85f895e771b7db2ae2
brian fan and zach beecher <brian+zach@>
2017-07-11 10:19:21 -0700

    initial commit

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
   def clean
     current_cart.clean!
-    flash[:warning] = "已清空购物车"
+    flash[:warning] = "Your Cart is empty now"
     redirect_to carts_path
   end
 end
